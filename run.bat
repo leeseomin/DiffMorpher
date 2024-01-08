28af0b2455108abc25df101afda7feece05c7e5c
LEE SEOMIN <86279699+leeseomin@users.noreply.github.com>
2024-01-08 12:22:56 +0900

    Add files via upload

--- run.bat
+++ run.bat
@@ -2,4 +2,6 @@
 python -m venv myenv
 call myenv\Scripts\activate
 pip install -r requirements.txt
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 python app.py
+
